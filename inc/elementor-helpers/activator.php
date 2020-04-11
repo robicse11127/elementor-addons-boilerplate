39db661a7e2c813d1a738bcf737819d4e8054b51
robicse11127 <robicse11127@gmail.com>
2020-04-11 14:37:08 +0600

    inc folder removed

--- inc/elementor-helpers/activator.php
+++
@@ -1,5 +0,0 @@
-<?php
-function prefix_elements_activator() {
-	require_once PREFIX_PLUGIN_PATH . '/elements/blank.php';
-}
-add_action('elementor/widgets/widgets_registered','prefix_elements_activator');
